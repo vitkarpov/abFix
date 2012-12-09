867925becae35dcf3c1b19d008db0890966f24d2
Vit Karpov <viktor.s.karpov@gmail.com>
2012-12-09 20:28:26 +0300

    added demo page plugin source is inside the js folder

---
+++ jquery.afterFix.js
@@ -0,0 +1,17 @@
+(function( $ ) {
+  $.fn.abFix = function( options ) {
+  	var settings = $.extend( {
+      beforeSelector : 'before-fix',
+      afterSelector  : 'after-fix',
+      beforeClass    : 'ie-prepend',
+      afterClass     : 'ie-append'
+    }, options);
+
+  	console.log('1');
+
+  	return this.each(function() {
+  		$(this).find( '.' + settings.beforeSelector ).prepend( '<i class="' + settings.beforeClass + '" />' );
+			$(this).find( '.' + settings.afterSelector ).append( '<i class="' + settings.afterClass + '" />' );
+  	});
+  };
+})( jQuery );
